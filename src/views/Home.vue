e55ef7ae3a978b6202385cdbdb6e541fa44035b6
Maciej <radymmaciej@gmail.com>
2020-09-04 19:16:37 +0200

    tak

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,47 +32,4 @@ export default {
 }
 </script>
 
-<<<<<<< HEAD
-<style lang="scss" scoped src="./Home.scss"></style>
-=======
-<style lang="scss" scoped>
-.console {
-  height: calc(100vh - (1.6rem));
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  $fontSize: 1.6rem;
-  font-weight: normal;
-  font-family: monospace;
-  background-color: #fff;
-  padding: 0;
-  border: 0;
-
-  &__textarea {
-    flex-grow: 1;
-  }
-
-  &__input {
-    border: 0;
-    padding: 1px 0;
-    width: 100%;
-    font-size: $fontSize;
-    font-family: inherit;
-  }
-
-  &__prompt-wrapper {
-    position: absolute;
-    bottom: 2px;
-    color: transparent;
-    user-select: none;
-    pointer-events: none;
-    width: 100%;
-    font-size: $fontSize;
-  }
-
-  &__prompt {
-    color: #000;
-  }
-}
-</style>
->>>>>>> 616fac3ebb68314477b2a6e3e2c9b8131e21966d
+<style lang="scss" scoped></style>
