69b667855e7439385a35ff522f472342a5440363
Dodonito <dominik14081999@wp.pl>
2020-09-05 16:25:15 +0200

    hamburger start

--- src/views/game.vue
+++ src/views/game.vue
@@ -24,6 +24,15 @@ export default {
         }
       })
       this.inputValue = '';
+    },
+    hamburgerShowHide() {
+      const hamburger = document.querySelector('.hamburger');
+      const nav = document.querySelector('.navigation');
+      const handleClick = () => {
+        hamburger.classList.toggle('hamburger--active');
+        nav.classList.toggle('navigation--active');
+      }
+      hamburger.addEventListener('click', handleClick);
     }
   },
   mounted() {
