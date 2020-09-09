f3705b2f67617961b496b4b9f59d55fb891443ea
Dodonito <dominik14081999@wp.pl>
2020-09-09 13:14:38 +0200

    cleaning up code

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ export class Api {
     static fetch(...args: Parameters<typeof fetch>) {
         const [url, options] = args;
 
-        return fetch('http://192.168.0.100:4000/' + url, {
+        return fetch('http://localhost:4000/' + url, {
             headers: new Headers({
                 'Content-Type': 'application/json'
             }),
